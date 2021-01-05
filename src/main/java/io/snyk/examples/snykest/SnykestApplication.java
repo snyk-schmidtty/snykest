2678f91dd6376da49dcf46b28d886568b1e118bd
Gareth Rushgrove <gareth@morethanseven.net>
2021-01-05 10:13:51 +0000

    Working Sprint Boot application

---
+++ src/main/java/io/snyk/examples/snykest/SnykestApplication.java
@@ -0,0 +1,13 @@
+package io.snyk.examples.snykest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnykestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnykestApplication.class, args);
+	}
+
+}
---
+++ test/java/io/snyk/examples/snykest/SnykestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.snyk.examples.snykest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnykestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
